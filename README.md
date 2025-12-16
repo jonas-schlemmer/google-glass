a7eb2322f116c65dee28cce9e1acf85c6ae4c9fa
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2025-12-16 10:53:50 -0300

    Fixed many layout bugs

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# google-glass
- 
+# 📝 google-glass
+Projeto do Curso de HTML5 do Curso em Vídeo, ministrado pelo Gustavo Guanabara em 2013.
+
+## 🛠️ Tecnologias
+- HTML5;
+- CSS3;
+- JavaScript.
+
+## ✅ Funcionalidades
+- criarEditarEApagarTarefas;
+- filtrosPorColuna;
+- exportarParaExcel.
+
+## 🖼️ Telas
+![nomeTela](images/layout-nomeTelaIgualAoArquivo.png)
+![nomeTela](images/layout-nomeTelaIgualAoArquivo.png)
+![nomeTela](images/layout-nomeTelaIgualAoArquivo.png)
+
+## 🚀 Como Executar
+### Acesso web:
 https://jonas-schlemmer.github.io/google-glass/
+
+## 👤 Autores
+- [Jonas Schlemmer](https://github.com/jonas-schlemmer)
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
 function changePhoto(photo) {
-    document.getElementById('icon').src = photo;
+    document.getElementById('icon').src = photo
+}
+
+function calcTotal() {
+    // Faz a verificação se a quantidade é maior que zero, para não retornar NaN para o input
+    document.getElementById('quantity').addEventListener('input', function () {
+        let quantity = document.getElementById('quantity').value
+        document.getElementById('total-price').value = quantity ? parseInt(quantity) * 1500 : 0
+    })
+
+    // Faz a verificação se a quantidade é maior que zero, para não retornar NaN para o click
+    document.getElementById('quantity').addEventListener('click', function () {
+        let quantity = document.getElementById('quantity').value
+        document.getElementById('total-price').value = quantity ? parseInt(quantity) * 1500 : 0
+    })
 }
